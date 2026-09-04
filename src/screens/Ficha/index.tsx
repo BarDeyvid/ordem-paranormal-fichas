e830d6853816951da86e5ea54b058a565d78f6b7
quackthalia <naninathbruch@gmail.com>
2026-09-04 09:06:00 -0300

    style(atributos): lower PRE and VIG slightly to find vertical sweet spot

--- src/screens/Ficha/index.tsx
+++ src/screens/Ficha/index.tsx
@@ -114,8 +114,8 @@ function AtributosFicha() {
         
         {renderAtributo('AGI', 'top-[16%] left-[50%] -translate-x-1/2 -translate-y-1/2')}
         {renderAtributo('INT', 'top-[36%] right-[19%] translate-x-1/2 -translate-y-1/2')}
-        {renderAtributo('VIG', 'top-[72%] right-[27%] translate-x-1/2 -translate-y-1/2')}
-        {renderAtributo('PRE', 'top-[72%] left-[27%] -translate-x-1/2 -translate-y-1/2')}
+        {renderAtributo('VIG', 'top-[74%] right-[27%] translate-x-1/2 -translate-y-1/2')}
+        {renderAtributo('PRE', 'top-[74%] left-[27%] -translate-x-1/2 -translate-y-1/2')}
         {renderAtributo('FOR', 'top-[36%] left-[19%] -translate-x-1/2 -translate-y-1/2')}
       </div>
     </div>
