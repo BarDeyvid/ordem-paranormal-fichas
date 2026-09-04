b144624e476457c1a23b783adb5d526335422015
quackthalia <naninathbruch@gmail.com>
2026-09-04 08:45:35 -0300

    style(atributos): nudge FOR and INT slightly higher and more towards center

--- src/screens/Ficha/index.tsx
+++ src/screens/Ficha/index.tsx
@@ -113,10 +113,10 @@ function AtributosFicha() {
         <img src="/images/atributos-bg.png" alt="Atributos" className="w-full h-full object-contain pointer-events-none drop-shadow-[0_0_20px_rgba(255,255,255,0.05)]" />
         
         {renderAtributo('AGI', 'top-[16%] left-[50%] -translate-x-1/2 -translate-y-1/2')}
-        {renderAtributo('INT', 'top-[39%] right-[15%] translate-x-1/2 -translate-y-1/2')}
+        {renderAtributo('INT', 'top-[36%] right-[19%] translate-x-1/2 -translate-y-1/2')}
         {renderAtributo('VIG', 'top-[78%] right-[28%] translate-x-1/2 -translate-y-1/2')}
         {renderAtributo('PRE', 'top-[78%] left-[28%] -translate-x-1/2 -translate-y-1/2')}
-        {renderAtributo('FOR', 'top-[39%] left-[15%] -translate-x-1/2 -translate-y-1/2')}
+        {renderAtributo('FOR', 'top-[36%] left-[19%] -translate-x-1/2 -translate-y-1/2')}
       </div>
     </div>
   );
