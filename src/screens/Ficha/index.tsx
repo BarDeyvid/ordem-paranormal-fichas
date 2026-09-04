61838b1a50ccb73577c1a25348b58861199b40b6
quackthalia <naninathbruch@gmail.com>
2026-09-04 08:06:11 -0300

    style(atributos): adjust layout positioning for attribute values in pentagram diagram

--- src/screens/Ficha/index.tsx
+++ src/screens/Ficha/index.tsx
@@ -112,11 +112,11 @@ function AtributosFicha() {
       <div className="relative w-full max-w-[340px] aspect-square">
         <img src="/images/atributos-bg.png" alt="Atributos" className="w-full h-full object-contain pointer-events-none drop-shadow-[0_0_20px_rgba(255,255,255,0.05)]" />
         
-        {renderAtributo('AGI', 'top-[12%] left-[50%] -translate-x-1/2 -translate-y-1/2')}
-        {renderAtributo('INT', 'top-[39%] right-[11%] translate-x-1/2 -translate-y-1/2')}
-        {renderAtributo('VIG', 'top-[82%] right-[24%] translate-x-1/2 -translate-y-1/2')}
-        {renderAtributo('PRE', 'top-[82%] left-[24%] -translate-x-1/2 -translate-y-1/2')}
-        {renderAtributo('FOR', 'top-[39%] left-[11%] -translate-x-1/2 -translate-y-1/2')}
+        {renderAtributo('AGI', 'top-[16%] left-[50%] -translate-x-1/2 -translate-y-1/2')}
+        {renderAtributo('INT', 'top-[39%] right-[15%] translate-x-1/2 -translate-y-1/2')}
+        {renderAtributo('VIG', 'top-[78%] right-[28%] translate-x-1/2 -translate-y-1/2')}
+        {renderAtributo('PRE', 'top-[78%] left-[28%] -translate-x-1/2 -translate-y-1/2')}
+        {renderAtributo('FOR', 'top-[39%] left-[15%] -translate-x-1/2 -translate-y-1/2')}
       </div>
     </div>
   );
