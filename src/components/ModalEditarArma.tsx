7d6b56eeb3caa8db51c97bcebd6cd217caeec19d
quackthalia <naninathbruch@gmail.com>
2026-09-22 07:34:36 -0300

    fix(ui): corrige ReferenceError val is not defined no getEspacoNumber

--- src/components/ModalEditarArma.tsx
+++ src/components/ModalEditarArma.tsx
@@ -99,7 +99,7 @@ export function ModalEditarArma({
 
   const editorDesc = useRef<HTMLDivElement | null>(null);
 
-  function getEspacoNumber(str: string | number) { val = Number(String(str).replace(',', '.').replace(/[^0-9.-]+/g, ''));
+  function getEspacoNumber(str: string | number) { const val = Number(String(str).replace(',', '.').replace(/[^0-9.-]+/g, ''));
     return isNaN(val) ? 0 : val;
   };
 
